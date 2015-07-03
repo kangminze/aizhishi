a352e621576bd998e40f14d419a558f5aef04c86
kxz10323 <kang1032355997@126.com>
2015-07-03 15:58:37 +0800

    后台返回界面值

--- src/main/java/com/kxz/aizhishi/controller/HelloWorldController.java
+++ src/main/java/com/kxz/aizhishi/controller/HelloWorldController.java
@@ -3,20 +3,18 @@ package com.kxz.aizhishi.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-@org.springframework.stereotype.Controller
+@Controller
 @RequestMapping("/hello")
 public class HelloWorldController{
 
 	@RequestMapping("/nihao")
 	public String helloworld(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("message", "Hello World!");
-		mv.setViewName("main");
-		System.out.println("testsafasdfasdfadfasdfasfasf");
+			HttpServletResponse arg1,ModelMap model) throws Exception {
+		model.addAttribute("hello", "你好!");
 		return "main";
 	}
 
