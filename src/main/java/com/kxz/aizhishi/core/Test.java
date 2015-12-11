1fdd729a3660df6fdfe6447f416a71e1be806a30
kxz10323 <kang1032355997@126.com>
2015-12-11 22:03:59 +0800

    增加angularjs支持

---
+++ src/main/java/com/kxz/aizhishi/core/Test.java
@@ -0,0 +1,13 @@
+package com.kxz.aizhishi.core;
+
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		Object abc = null;
+		double ddd = (Double)abc;
+		System.out.println(ddd);
+	}
+}
